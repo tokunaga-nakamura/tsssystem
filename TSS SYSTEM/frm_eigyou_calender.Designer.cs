30c2a81a98928eb6545f2248e2f55ec0227927cd
nakamura@tokunaga-seisakusyo.co.jp <nakamura@tokunaga-seisakusyo.co.jp>
2015-08-07 16:59:37 +0900

    営業カレンダーをメニュに追加する前のコミット

---
+++ TSS SYSTEM/frm_eigyou_calender.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace TSS_SYSTEM
+{
+    partial class frm_eigyou_calender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_eigyou_calender));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btn_hardcopy = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.btn_syuuryou = new System.Windows.Forms.Button();
+            this.btn_touroku = new System.Windows.Forms.Button();
+            this.nud_year = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nud_month = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dgv_calender = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_year)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_month)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_calender)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(884, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btn_hardcopy);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(884, 539);
+            this.splitContainer1.SplitterDistance = 63;
+            this.splitContainer1.TabIndex = 1;
+            this.splitContainer1.TabStop = false;
+            // 
+            // btn_hardcopy
+            // 
+            this.btn_hardcopy.Image = ((System.Drawing.Image)(resources.GetObject("btn_hardcopy.Image")));
+            this.btn_hardcopy.Location = new System.Drawing.Point(12, 12);
+            this.btn_hardcopy.Name = "btn_hardcopy";
+            this.btn_hardcopy.Size = new System.Drawing.Size(36, 36);
+            this.btn_hardcopy.TabIndex = 0;
+            this.btn_hardcopy.UseVisualStyleBackColor = true;
+            this.btn_hardcopy.Click += new System.EventHandler(this.btn_hardcopy_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.btn_touroku);
+            this.splitContainer2.Panel2.Controls.Add(this.btn_syuuryou);
+            this.splitContainer2.Size = new System.Drawing.Size(884, 472);
+            this.splitContainer2.SplitterDistance = 436;
+            this.splitContainer2.TabIndex = 0;
+            this.splitContainer2.TabStop = false;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer3.Panel1.Controls.Add(this.nud_month);
+            this.splitContainer3.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer3.Panel1.Controls.Add(this.nud_year);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.dgv_calender);
+            this.splitContainer3.Size = new System.Drawing.Size(884, 436);
+            this.splitContainer3.SplitterDistance = 28;
+            this.splitContainer3.TabIndex = 0;
+            this.splitContainer3.TabStop = false;
+            // 
+            // btn_syuuryou
+            // 
+            this.btn_syuuryou.Location = new System.Drawing.Point(795, 3);
+            this.btn_syuuryou.Name = "btn_syuuryou";
+            this.btn_syuuryou.Size = new System.Drawing.Size(75, 23);
+            this.btn_syuuryou.TabIndex = 0;
+            this.btn_syuuryou.Text = "終了";
+            this.btn_syuuryou.UseVisualStyleBackColor = true;
+            this.btn_syuuryou.Click += new System.EventHandler(this.btn_syuuryou_Click);
+            // 
+            // btn_touroku
+            // 
+            this.btn_touroku.Location = new System.Drawing.Point(10, 3);
+            this.btn_touroku.Name = "btn_touroku";
+            this.btn_touroku.Size = new System.Drawing.Size(75, 23);
+            this.btn_touroku.TabIndex = 1;
+            this.btn_touroku.Text = "登録";
+            this.btn_touroku.UseVisualStyleBackColor = true;
+            // 
+            // nud_year
+            // 
+            this.nud_year.Location = new System.Drawing.Point(12, 3);
+            this.nud_year.Maximum = new decimal(new int[] {
+            2099,
+            0,
+            0,
+            0});
+            this.nud_year.Minimum = new decimal(new int[] {
+            2015,
+            0,
+            0,
+            0});
+            this.nud_year.Name = "nud_year";
+            this.nud_year.Size = new System.Drawing.Size(54, 19);
+            this.nud_year.TabIndex = 0;
+            this.nud_year.Value = new decimal(new int[] {
+            2015,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.textBox1.Location = new System.Drawing.Point(66, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(23, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "年";
+            // 
+            // nud_month
+            // 
+            this.nud_month.Location = new System.Drawing.Point(95, 3);
+            this.nud_month.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.nud_month.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_month.Name = "nud_month";
+            this.nud_month.Size = new System.Drawing.Size(38, 19);
+            this.nud_month.TabIndex = 2;
+            this.nud_month.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.NavajoWhite;
+            this.textBox2.Location = new System.Drawing.Point(133, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(23, 19);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "月";
+            // 
+            // dgv_calender
+            // 
+            this.dgv_calender.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_calender.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_calender.Location = new System.Drawing.Point(0, 0);
+            this.dgv_calender.Name = "dgv_calender";
+            this.dgv_calender.RowTemplate.Height = 21;
+            this.dgv_calender.Size = new System.Drawing.Size(880, 400);
+            this.dgv_calender.TabIndex = 0;
+            // 
+            // frm_eigyou_calender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "frm_eigyou_calender";
+            this.Text = "営業カレンダー";
+            this.Load += new System.EventHandler(this.frm_eigyou_calender_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_year)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_month)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_calender)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btn_hardcopy;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button btn_syuuryou;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown nud_month;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown nud_year;
+        private System.Windows.Forms.DataGridView dgv_calender;
+        private System.Windows.Forms.Button btn_touroku;
+    }
+}
---
+++ TSS SYSTEM/frm_eigyou_calender.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSS_SYSTEM
+{
+    public partial class frm_eigyou_calender : Form
+    {
+        TssSystemLibrary tss = new TssSystemLibrary();
+        DataTable w_dt_calender = new DataTable();
+
+        public frm_eigyou_calender()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_hardcopy_Click(object sender, EventArgs e)
+        {
+            tss.HardCopy();
+        }
+
+        private void btn_syuuryou_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frm_eigyou_calender_Load(object sender, EventArgs e)
+        {
+            //表示の初期値設定
+            nud_year.Value = DateTime.Today.Year;
+            nud_month.Value = DateTime.Today.Month;
+            //表示
+            calender_disp();
+        }
+
+
+        private void calender_disp()
+        {
+            get_calender();
+            if(w_dt_calender.Rows.Count == 0)
+            {
+                //新規にレコードを作成
+                calender_create();
+                get_calender();
+                if(w_dt_calender.Rows.Count == 0)
+                {
+                    MessageBox.Show("営業カレンダーの作成で例外エラーが発生しました。処理を中止します。");
+                    this.Close();
+                }
+            }
+            dgv_calender.DataSource = w_dt_calender;
+
+            //セルの高さ変更不可
+            dgv_calender.AllowUserToResizeRows = false;
+
+            //カラムヘッダーの高さ変更不可
+            dgv_calender.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+
+            //削除不可にする（コードからは削除可）
+            dgv_calender.AllowUserToDeleteRows = false;
+
+            //行ヘッダーを非表示にする
+            dgv_calender.RowHeadersVisible = false;
+
+            //DataGridView1にユーザーが新しい行を追加できないようにする（最下行を非表示にする）
+            dgv_calender.AllowUserToAddRows = false;
+
+            //列の文字数制限（TextBoxのMaxLengthと同じ動作になる）
+            ((DataGridViewTextBoxColumn)dgv_calender.Columns[3]).MaxInputLength = 1;    //祝祭日名称
+            ((DataGridViewTextBoxColumn)dgv_calender.Columns[4]).MaxInputLength = 1;    //営業区分
+            ((DataGridViewTextBoxColumn)dgv_calender.Columns[7]).MaxInputLength = 1;    //内容
+
+            //指定列を非表示にする
+            dgv_calender.Columns[8].Visible = false;
+            dgv_calender.Columns[9].Visible = false;
+            dgv_calender.Columns[10].Visible = false;
+            dgv_calender.Columns[11].Visible = false;
+        }
+
+
+        private void calender_create()
+        {
+            tss.GetUser();
+            int w_int_days = DateTime.DaysInMonth((int)nud_year.Value,(int)nud_month.Value);
+            for(int i = 1;i <= w_int_days;i++)
+            {
+                if(tss.OracleInsert("insert into tss_calender_f (year,month,day,create_user_cd,create_datetime) value ('" + nud_year.Value.ToString() + "','" + nud_month.Value.ToString("00") + "','" + i.ToString("00") + "','" + tss.user_cd + "',sysdate") == false)
+                {
+                    MessageBox.Show("営業カレンダーの作成でエラーが発生しました。処理を中止します。");
+                    this.Close();
+                }
+            }
+        }
+
+        private void get_calender()
+        {
+            w_dt_calender = tss.OracleSelect("select * from tss_calender_f where year = '" + nud_year.Value.ToString() + "' and month = '" + nud_month.Value.ToString("00") + "'");
+        }
+
+    }
+}
